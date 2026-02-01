85aed37d8fc09b808af70b826d58b62082c29d55
François Gibier <francoisgibier.contact@gmail.com>
2026-02-01 21:24:36 +0100

    refactor: uncoupled rapl config from profiler config, added errors at initialization to avoid executing profiler when an error occured

--- cli/src/bin/main.rs
+++ cli/src/bin/main.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use joule_profiler_cli::{CliArgs, output_format_to_displayer, parse_config};
+use joule_profiler_cli::{CliArgs, ProfilerCommand, output_format_to_displayer, parse_config};
 use joule_profiler_core::JouleProfiler;
 use joule_profiler_core::config::{Command, Config};
 use source_rapl::Rapl;
@@ -8,11 +8,22 @@ use source_rapl::Rapl;
 async fn main() -> Result<()> {
     let cli = CliArgs::from_args()?;
     let mut displayer = output_format_to_displayer(&cli)?;
-    let config: Config = parse_config(cli)?;
 
-    let rapl = Rapl::try_from(&config)?;
+    let rapl_polling = match &cli.command {
+        ProfilerCommand::Phases(phases_args) => phases_args.rapl_polling,
+        ProfilerCommand::ListSensors => None,
+    };
+
+    let rapl = Rapl::new(
+        cli.rapl_path.as_deref(),
+        cli.sockets.as_deref(),
+        rapl_polling,
+    )?;
+
     let mut profiler = JouleProfiler::new();
     profiler.add_source(rapl);
+
+    let config: Config = parse_config(cli)?;
 
     match config.command {
         Command::Profile(profile_config) => {
