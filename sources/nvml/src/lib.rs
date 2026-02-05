41b735f43508536e43a670966f3b7be559954ce1
François Gibier <francoisgibier.contact@gmail.com>
2026-02-05 13:44:24 +0100

    chore: cargo fmt

--- sources/nvml/src/lib.rs
+++ sources/nvml/src/lib.rs
@@ -70,12 +70,10 @@ impl Nvml {
     /// * No GPU devices are detected
     pub fn new() -> Result<Self> {
         debug!("Attempting to initialize NVML reader");
-        let nvml = nvml_wrapper::Nvml::init().map_err(|err| {
-            match err {
-                nvml_wrapper::error::NvmlError::DriverNotLoaded => NvmlError::NoDriverLoaded,
-                nvml_wrapper::error::NvmlError::NoPermission => NvmlError::NoPermission,
-                _ => err.into()
-            }
+        let nvml = nvml_wrapper::Nvml::init().map_err(|err| match err {
+            nvml_wrapper::error::NvmlError::DriverNotLoaded => NvmlError::NoDriverLoaded,
+            nvml_wrapper::error::NvmlError::NoPermission => NvmlError::NoPermission,
+            _ => err.into(),
         })?;
         let devices_max_index = nvml.device_count()?;
         for i in 0..devices_max_index {
