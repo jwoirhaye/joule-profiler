7c0e9ff123bac3514dd8a4bab1a99b8c07d81120
François Gibier <francoisgibier.contact@gmail.com>
2026-02-05 10:06:23 +0100

    docs: nvml source doc added

--- core/src/source/reader.rs
+++ core/src/source/reader.rs
@@ -29,6 +29,8 @@ use crate::source::{MetricReaderErrorBound, MetricReaderTypeBound};
 /// - [`MetricReader::init`] — Source initialization logic if there is one, called before the measurements.
 /// - [`MetricReader::join`] — Source destruction logic if there is one, called before the measurements (no Drop implementation because the source is reusable).
 ///   Calling init and join several times should always behave the same, the source shall be reset when joining.
+/// - [`MetricReader::reset`] — Reset the source counters, called before the measurements, it is useful if a source implements polling and
+///   measures have already been made before measurements.
 pub trait MetricReader: Send + 'static {
     /// Type of metrics returned by the reader.
     type Type: MetricReaderTypeBound;
@@ -36,7 +38,7 @@ pub trait MetricReader: Send + 'static {
     /// Error type produced by the reader.
     type Error: MetricReaderErrorBound;
 
-    /// Init the source if it implements custom logic underneath
+    /// Init the source if it implements custom logic underneath.
     fn init(&mut self) -> impl Future<Output = Result<(), Self::Error>> + Send {
         async { Ok(()) }
     }
@@ -46,21 +48,23 @@ pub trait MetricReader: Send + 'static {
         async { Ok(()) }
     }
 
-    /// Measure the sensors metrics and update internal state
+    /// Measure the sensors metrics and update internal state.
     fn measure(&mut self) -> impl Future<Output = Result<(), Self::Error>> + Send;
 
-    /// Reset the source counters
-    fn reset(&mut self) -> impl Future<Output = Result<(), Self::Error>> + Send;
+    /// Reset the source counters if it implements custom logic underneath.
+    fn reset(&mut self) -> impl Future<Output = Result<(), Self::Error>> + Send {
+        async { Ok(()) }
+    }
 
-    /// Retrieve the current metrics as the reader type
+    /// Retrieve the current metrics as the reader type.
     fn retrieve(&mut self) -> impl Future<Output = Result<Self::Type, Self::Error>> + Send;
 
-    /// Return all sensors available from this reader
+    /// Return all sensors available from this reader.
     fn get_sensors(&self) -> Result<Sensors, Self::Error>;
 
-    /// Convert the metric reader data to metrics
+    /// Convert the metric reader data to metrics.
     fn to_metrics(&self, result: Self::Type) -> Metrics;
 
-    /// Get the name of the metric source
+    /// Get the name of the metric source.
     fn get_name() -> &'static str;
 }
