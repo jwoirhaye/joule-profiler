b6aca8e16cd97be84196f2273ae1e016b70e13bc
François Gibier <francoisgibier.contact@gmail.com>
2026-03-24 10:02:04 +0100

    chore: removed must_use compilation flags added by clippy

--- core/src/util/fs.rs
+++ core/src/util/fs.rs
@@ -20,7 +20,6 @@ pub fn get_absolute_path(filename: &str) -> Result<String, std::io::Error> {
 }
 
 /// Generates a default filename for iteration data.
-#[must_use]
 pub fn default_iterations_filename(ext: &str) -> String {
     format!("data{}.{}", get_timestamp_millis(), ext)
 }
