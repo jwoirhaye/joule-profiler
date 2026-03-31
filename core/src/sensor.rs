b01a6f02da6f8c0df1f8f98a423eb2acb7984426
François Gibier <francoisgibier.contact@gmail.com>
2026-03-31 14:44:44 +0200

    test: test NVML source using generics (doesn't need to change the public API with default generics), and mock with mockall

--- core/src/sensor.rs
+++ core/src/sensor.rs
@@ -34,7 +34,7 @@ use crate::unit::MetricUnit;
 /// assert_eq!(sensor.unit.to_string(), "µJ");
 /// assert_eq!(sensor.source, "powercap");
 /// ```
-#[derive(Debug, Serialize, PartialEq, Eq)]
+#[derive(Debug, Serialize, Clone, PartialEq, Eq)]
 pub struct Sensor {
     /// The name of the sensor.
     pub name: String,
