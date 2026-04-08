62388cb2751ddf35c3a900b6dde07b4330c460fe
François Gibier <francoisgibier.contact@gmail.com>
2026-04-08 10:21:17 +0200

    feat: added MetricValue to support various metric types

--- core/src/aggregate/metric.rs
+++ core/src/aggregate/metric.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use serde::Serialize;
 
 use crate::unit::MetricUnit;
@@ -22,7 +24,7 @@ pub struct Metric {
     pub name: String,
 
     /// The numeric value of the metric.
-    pub value: u64,
+    pub value: MetricValue,
 
     /// The unit of measurement.
     pub unit: MetricUnit,
@@ -31,5 +33,55 @@ pub struct Metric {
     pub source: String,
 }
 
+impl Metric {
+    pub fn new<T>(name: String, value: T, unit: MetricUnit, source: String) -> Self
+    where
+        T: Into<MetricValue>,
+    {
+        Metric {
+            name,
+            value: value.into(),
+            unit,
+            source,
+        }
+    }
+}
+
 /// A collection of metrics.
 pub type Metrics = Vec<Metric>;
+
+/// Enum representing the value of a metric,
+/// with this enum, a metric can be a signed or
+/// unsigned integer or a float.
+#[derive(Debug, Serialize, Clone, Copy, PartialEq)]
+pub enum MetricValue {
+    UnsignedInteger(u64),
+    SignedInteger(i64),
+    Float(f64),
+}
+
+impl From<u64> for MetricValue {
+    fn from(v: u64) -> Self {
+        Self::UnsignedInteger(v)
+    }
+}
+impl From<i64> for MetricValue {
+    fn from(v: i64) -> Self {
+        Self::SignedInteger(v)
+    }
+}
+impl From<f64> for MetricValue {
+    fn from(v: f64) -> Self {
+        Self::Float(v)
+    }
+}
+
+impl Display for MetricValue {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::UnsignedInteger(v) => v.fmt(f),
+            Self::SignedInteger(v) => v.fmt(f),
+            Self::Float(v) => v.fmt(f),
+        }
+    }
+}
